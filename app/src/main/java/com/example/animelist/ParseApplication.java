d00394a71c81bd90bb707035feeee0a3d457a4a3
Raymond <rkwok7877@gmail.com>
2021-04-21 11:18:29 -0400

    added ParseApplication API for back4app

---
+++ app/src/main/java/com/example/animelist/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.animelist;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("fwFDQYObut4lJYaA10gMBtJeeCLFxJLDnIcE4xv7")
+                .clientKey("k9gF9jLlUVDVPT2akZS8tEPpl67yviTAWP7FFUUU")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
+
+
+
