827a8f9083aabbf2b7e427fa65846ceff19dc34b
Brian To <bto2442@bths.edu>
2021-04-28 10:35:36 -0400

    - Made Anime model Class - Started setting up AnimeAdapter

--- app/src/main/java/com/example/animelist/MainActivity.java
+++ app/src/main/java/com/example/animelist/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.animelist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.animelist.model.Anime;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Anime> animes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvAnime = findViewById(R.id.rvAnimes);
+        animes = new ArrayList<>();
+
+
     }
 }
---
+++ app/src/main/java/com/example/animelist/model/Anime.java
@@ -0,0 +1,13 @@
+package com.example.animelist.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Anime {
+
+    public Anime(){}
+
+    public Anime(JSONObject jsonObject) throws JSONException{
+
+    }
+}
